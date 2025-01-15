0993df253d4cbd0e02dfbb755f532aa26da46131
Steve Chin <schin8@bu.edu>
2025-01-15 14:16:53 -0500

    Added selenium dependencies, added log4j.xml to get rid of annoying message, Sanity check Selenium in JUnit

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,19 +1,53 @@
 package org.example;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
+    public WebDriver driver;
+    public static final String BASEURL = "https://simplisafe.com/";
 
+    @BeforeEach
+    public void testSetup(){
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+//        options.addArguments("--headless");
+//        options.addArguments("--incognito");
+        driver = new ChromeDriver(options);
+
+        driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get(BASEURL);
+
+    }
+
+    @AfterEach
+    public void testCleanup() {
+        driver.close();
+    }
+
+    @Disabled
     @Test
     public void sanityCheck(){
         Main testTarget = new Main();
         assertEquals(1, testTarget.sanity_check());
     }
 
-    @Test
-    public void pullRequestTest(){
+    @Test void seleniumSetupSanityCheck(){
         Main testTarget = new Main();
         assertEquals(1, testTarget.sanity_check());
     }
