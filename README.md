0357bbce2556100d9c2487f1435439d1126c698b
Steve Chin <schin8@bu.edu>
2025-01-15 11:21:55 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# simplisafe-wordle
---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    public void sanityCheck(){
+        Main testTarget = new Main();
+
+        assertEquals(1, testTarget.sanity_check());
+    }
+}
